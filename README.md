ec4c5e260b59170b6d517c7ea13bad4778c04264
Marc ALEXANDRE <marc@lumapps.com>
2014-11-10 03:04:46 +0100

    Add "OPTIMIZE_ONCE" option to optimize directly the content folder instead of output

--- README.md
+++ README.md
@@ -24,3 +24,10 @@ See the [pelican documentation](http://docs.getpelican.com/en/latest/plugins.htm
 ## Usage
 
 All the GIF, PNG & JPEG images of your blog will be optimized during the build.
+Adding `IMAGE_OPTIMIZATION_ONCE_AND_FOR_ALL = True` to you `pelicanconf.py` will optimize the images in the content folder directly. The files will be renamed `_optimized`, but once in the output folder, they will be renamed without the `_optimized`.
+The markdown files need to use the name without `_optimized`.
+
+Example:
+1. I write the article `test` with an image `content/img/test.png` linked in the template and saved in my content folder.
+2. On pelican generation, the file is optimized in my content folder and renamed `test_optimized.png`. In my output folder, the file is `test.png`, so my markdown link still works.
+3. On the next pelican generation, the file won't be optimized, reducing the time for the generation.
--- pelican_image_optimizer.py
+++ pelican_image_optimizer.py
@@ -2,7 +2,7 @@
 
 """
 Optimized images (gif, jpeg & png)
-Assumes that Gifsicle, ImageMagick's convert and pngquant are isntalled on path.
+Assumes that Gifsicle, ImageMagick's convert and pngquant are isntalled on path
 http://www.lcdf.org/gifsicle/
 http://www.imagemagick.org/
 http://pngquant.org/
@@ -10,6 +10,7 @@ Copyright (c) 2014 Marc Alexandre (http://www.malexandre.fr)
 """
 
 import os
+import re
 import shutil
 from subprocess import call
 
@@ -25,19 +26,49 @@ COMMANDS = {
     '.gif': 'gifsicle --no-warnings -O "{filename}" -o "{filename}."'
 }
 
+OPTIMIZED = '_optimized'
+FLAG = 'IMAGE_OPTIMIZATION_ONCE_AND_FOR_ALL'
 
-def pelican_image_optimizer(pelican):
+
+def image_optimizer_initialized(pelican):
     """
-    Optimized gif, jpg and png images
+    Optimized gif, jpg and png images.
+
+    @param pelican: The Pelican instance
+    """
+    if not pelican.settings[FLAG]:
+        return
+
+    for dirpath, _, filenames in os.walk(pelican.settings['PATH']):
+        for name in filenames:
+            if os.path.splitext(name)[1] in COMMANDS.keys():
+                if not re.search(OPTIMIZED + r'\.(png|jpg|jpeg|gif)', name):
+                    optimize(pelican, dirpath, name)
+
+
+def image_optimizer_finalized(pelican):
+    """
+    Optimized gif, jpg and png images. If the
+    FLAG settings is set to True, just rename
+    the file alread optimized.
 
     @param pelican: The Pelican instance
     """
     for dirpath, _, filenames in os.walk(pelican.settings['OUTPUT_PATH']):
         for name in filenames:
             if os.path.splitext(name)[1] in COMMANDS.keys():
-                optimize(dirpath, name)
+                if pelican.settings[FLAG]:
+                    if '_optimized' in name:
+                        filepath = os.path.join(dirpath, name)
+                        newname = re.sub(OPTIMIZED + r'\.(png|jpg|jpeg|gif)',
+                                         r'.\1', name)
+                        newfilepath = os.path.join(dirpath, newname)
+                        shutil.move(filepath, newfilepath)
+                else:
+                    optimize(pelican, dirpath, name)
 
-def optimize(dirpath, filename):
+
+def optimize(pelican, dirpath, filename):
     """
     Optimize the image.
 
@@ -57,10 +88,15 @@ def optimize(dirpath, filename):
     else:
         os.remove(filepath + '.')
 
+    if pelican.settings[FLAG]:
+        new_name = re.sub(r'\.(png|jpg|jpeg|gif)',
+                          OPTIMIZED + r'.\1', filename)
+        shutil.move(filepath, os.path.join(dirpath, new_name))
+
 
 def register():
     """
     Register the plugin in Pelican.
     """
-    signals.finalized.connect(pelican_image_optimizer)
-
+    signals.initialized.connect(image_optimizer_initialized)
+    signals.finalized.connect(image_optimizer_finalized)
